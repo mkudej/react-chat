3b48dede6033c125657de7ee3d47e7095de2a9f7
Mateusz <kudej@hotelnetsolutions.de>
2021-08-24 00:19:22 +0200

    As user I can log in

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 export default function Card({ children }: CardProps) {
   return (
-    <div className="px-14 py-10 border-solid border-border-color border rounded-3xl max-w-md">
+    <div className="px-14 py-10 border-solid border-border-color border rounded-3xl max-w-md w-full">
       {children}
     </div>
   );
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 import "firebase/analytics";
 firebase.initializeApp({
-  apiKey: "AIzaSyC9qWwonmGmCsa_i2jqtCybdKH6PsvVqy4",
-  authDomain: "chatapp-5c062.firebaseapp.com",
-  projectId: "chatapp-5c062",
-  storageBucket: "chatapp-5c062.appspot.com",
-  messagingSenderId: "468485843459",
-  appId: "1:468485843459:web:0de3456f0d8eef644b3c36",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export const auth = firebase.auth();
