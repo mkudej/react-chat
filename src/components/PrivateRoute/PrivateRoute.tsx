7331546c5e32739e724bf979febd95b30723c7e4
Mateusz <kudej@hotelnetsolutions.de>
2021-08-31 21:50:08 +0200

    refactor: Fixed type for PrivateRoute props

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,10 +1,14 @@
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Redirect, Route } from "react-router";
+import { Redirect, Route, RouteProps } from "react-router";
 import { auth } from "../../firebase";
 
-export default function PrivateRoute({ component: Component, ...rest }: any) {
+export default function PrivateRoute({
+  component: Component,
+  ...rest
+}: RouteProps) {
   const [user, loading] = useAuthState(auth);
 
+  if (!Component) return null;
   if (loading) return <div>LOADING...</div>;
 
   return (
